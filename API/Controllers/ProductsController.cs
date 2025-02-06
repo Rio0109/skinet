495b34ab01a375b2fbc24387caf69928dd053df3
Rio0109 <Rio0109>
2025-02-06 18:05:58 +0100

    added sorting option

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -16,7 +16,7 @@ public class ProductsController(IGenericRepository<Product> repo) : ControllerBa
         string? type,string? sort)
    {
 
-        var spec = new ProductSpecification(brand,type);
+        var spec = new ProductSpecification(brand, type, sort);
 
         var products = await repo.ListAsync(spec);
 
--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9671f92d9cf5a85be36e814a4a9cb2a7b6d7bcfa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee91c9a47f01d7079e65575ed4358236a0fc5b6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
